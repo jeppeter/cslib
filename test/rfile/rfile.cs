dfe93eadab5f436274363ac0753e3ead15a4f176
jeppeter <jeppeter@gmail.com>
2018-06-01 15:43:26 +0800

    to add rfile

---
+++ test/rfile/rfile.cs
@@ -0,0 +1,22 @@
+using System.IO;
+using System;
+
+namespace ReadFile {
+	public class ReadFile {
+		public static void Main(String[] args) {
+			StreamReader reader;
+			String line;
+
+			foreach (string f in args) {
+				reader = new StreamReader(f);
+				Console.WriteLine("read {0} file", f);
+				Console.WriteLine("----------------------------");
+				while ((line = reader.ReadLine()) != null) {
+					Console.WriteLine(line);
+				}
+				reader.Close();
+				Console.WriteLine("++++++++++++++++++++++++++++");
+			}
+		}
+	}
+}
