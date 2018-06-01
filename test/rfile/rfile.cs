c5d59c8b3b66c8b3553bca5d6945fb7066bb0e00
jeppeter <jeppeter@gmail.com>
2018-06-01 15:48:29 +0800

    to make stream reader

--- test/rfile/rfile.cs
+++ test/rfile/rfile.cs
@@ -1,22 +1,37 @@
 using System.IO;
 using System;
 
-namespace ReadFile {
-	public class ReadFile {
-		public static void Main(String[] args) {
-			StreamReader reader;
-			String line;
+namespace ReadFile
+{
+public class ReadFile
+{
+    public static void Main(String[] args)
+    {
+        StreamReader reader;
+        String line;
 
-			foreach (string f in args) {
-				reader = new StreamReader(f);
-				Console.WriteLine("read {0} file", f);
-				Console.WriteLine("----------------------------");
-				while ((line = reader.ReadLine()) != null) {
-					Console.WriteLine(line);
-				}
-				reader.Close();
-				Console.WriteLine("++++++++++++++++++++++++++++");
-			}
-		}
-	}
+        if (args.Length > 0) {
+            foreach (string f in args) {
+                reader = new StreamReader(f);
+                Console.WriteLine("read {0} file", f);
+                Console.WriteLine("----------------------------");
+                while ((line = reader.ReadLine()) != null) {
+                    Console.WriteLine(line);
+                }
+                reader.Close();
+                Console.WriteLine("++++++++++++++++++++++++++++");
+            }
+        } else {
+            reader = new StreamReader(Console.OpenStandardInput());
+            Console.WriteLine("read stdin");
+            Console.WriteLine("----------------------------");
+            while ((line = reader.ReadLine()) != null) {
+                Console.WriteLine(line);
+            }
+            reader.Close();
+            Console.WriteLine("++++++++++++++++++++++++++++");
+        }
+
+    }
+}
 }
