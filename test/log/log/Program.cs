38e2f62e3140c7e4346d48b615db1acf3a839f67
jeppeter <jeppeter@gmail.com>
2018-06-19 15:53:26 +0800

    to make log4net run ok

---
+++ test/log/log/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Principal;
+using log4net;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private static void Main(string[] args)
+        {
+            Logger.InfoFormat("Running as {0}", WindowsIdentity.GetCurrent().Name);
+            Logger.Error("This will appear in red in the console and still be written to file!");
+            Console.WriteLine("please enter return to exit");
+            Console.ReadLine();
+        }
+    }
+}
