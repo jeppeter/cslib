0b351683b4184a018c090ae6ffcc4caf765a8233
jeppeter <jeppeter@gmail.com>
2018-06-25 15:21:19 +0800

    to add tstcase

---
+++ test/tstcase/tstcase.Test/tstcase.Test.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using tstcase;
+
+
+namespace tstcase.Test
+{
+    [TestFixture]
+    class tstcase_Test
+    {
+        private BaseCall* _mcall = null;
+        public tstcase_Test()
+        {
+            _mcall = new BaseCall();
+        }
+
+        [Test]
+        public void tstcase_Test_add()
+        {
+            var result = _mcall.BaseCall_add(1, 2);
+            Assert.That(result == 3);
+        }
+    }
+}
---
+++ test/tstcase/tstcase/BaseCall.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tstcase
+{
+    class BaseCall
+    {
+        public BaseCall()
+        {
+
+        }
+
+        public int BaseCall_add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int BaseCall_mul(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
