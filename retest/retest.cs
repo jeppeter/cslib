92f135375a1243b8c1203730bc1d08e45edcd6e7
jeppeter <jeppeter@gmail.com>
2018-06-29 15:25:01 +0800

    to add retest.cs

---
+++ retest/retest.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace retest
+{
+	public class retest
+	{
+		private string m_cmd;
+
+		private void Usage(int ec, string fmtstr, object[] fmtobj)
+		{
+			
+		}
+
+		public retest(string cmd)
+		{
+
+		}
+
+
+		public static void Main(string[] args)
+		{
+			if (args.Length < 2) {
+
+			}
+		}
+	}
+}
