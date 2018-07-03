9977336e46e10872c1e366dcc5f1a21cae2d62bc
jeppeter <jeppeter@gmail.com>
2018-07-03 16:33:37 +0800

    to make jsonutil

--- test/jsonutil/main.cs
+++ test/jsonutil/main.cs
@@ -1,15 +1,26 @@
 ﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
 
 namespace jsonutil
 {
-    class JsonUtil
+class JsonUtil
+{
+    private Dictionary<string, Object> m_obj;
+    public JsonUtil(string instr, bool filed = false)
     {
-    	public JsonUtil(string file)
-    	{
-    		
-    	}
-        static void Main(string[] args)
-        {
+        if (filed) {
+            this.m_obj = JsonConvert.DeserializeObject<Dictionary<string, Object>>(File.ReadAllText(instr));
+        } else {
+            this.m_obj = JsonConvert.DeserializeObject<Dictionary<string, Object>>(instr);
         }
     }
+
+    public Object 
+    static void Main(string[] args)
+    {
+    }
+}
 }
