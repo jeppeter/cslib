b8e67b0791206b9c1d4882b2b03502f7c7e1e1e1
jeppeter <jeppeter@gmail.com>
2018-07-03 17:09:53 +0800

    to make collection

--- test/jsonutil/main.cs
+++ test/jsonutil/main.cs
@@ -18,9 +18,38 @@ class JsonUtil
         }
     }
 
-    public Object 
+
+    public override string ToString()
+    {
+        Dictionary<string,Object>.KeyCollection keys;
+        string k;
+        int i;
+        string rets = "";
+        keys = this.m_obj.Keys;
+        rets += "{\n";
+        for (i=0;i<keys.Length;i++) {
+        	k = keys[i].Key;
+            rets += String.Format("\"{0}\" : {1}\n", k, this.m_obj[k]);
+        }
+        rets += "}\n";
+        return rets;
+    }
+
     static void Main(string[] args)
     {
+        JsonUtil util ;
+        int i;
+        if (args.Length > 0) {
+            if (args[i] == "parse") {
+                for (i = 1; i < args.Length; i++) {
+                    util = new JsonUtil(args[i], true);
+                    Console.Out.WriteLine("{0}", util.ToString());
+                }
+            } else {
+                throw new Exception(String.Format("unknown [{0}] command", args[0]));
+            }
+        }
+        return;
     }
 }
 }
