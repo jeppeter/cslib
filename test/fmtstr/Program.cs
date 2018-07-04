b94dc9be8cb092c4eea8451da73717a88c8b2da5
jeppeter <jeppeter@gmail.com>
2018-07-04 16:48:58 +0800

    to add fmtstr

---
+++ test/fmtstr/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace fmtstr
+{
+    class Program
+    {
+    	private static string format_string(string fmtstr, params string[] args)
+    	{
+    		return String.Format(fmtstr,args);
+    	}
+        static void Main(string[] args)
+        {
+            string rets;
+            string[] slicearr = {};
+            if (args.Length > 1) {
+            	slicearr = new string[(args.Length-1)];
+            }
+            Array.Copy(args,1,slicearr,0,args.Length - 1);
+            rets = format_string(args[0], slicearr);
+            Console.Out.Write(rets);
+            return;
+        }
+    }
+}
