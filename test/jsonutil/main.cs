75c352049a9dbcffe5002ae0ff6d3c671d8e83ab
jeppeter <jeppeter@gmail.com>
2018-07-04 14:37:22 +0800

    simple type output

--- test/jsonutil/main.cs
+++ test/jsonutil/main.cs
@@ -2,7 +2,6 @@
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
-using Newtonsoft.Json.Linq;
 
 
 /*****************************************
@@ -15,67 +14,14 @@ namespace jsonutil
 {
 class JsonUtil
 {
-    private JObject m_obj;
+    private Dictionary<string, Object> m_obj;
     public JsonUtil(string instr, bool filed = false)
     {
         if (filed) {
-            this.m_obj = JObject.Parse(File.ReadAllText(instr));
+            this.m_obj = JsonConvert.DeserializeObject<Dictionary<string, Object>>(File.ReadAllText(instr));
         } else {
-            this.m_obj = JObject.Parse(instr);
+            this.m_obj = JsonConvert.DeserializeObject<Dictionary<string, Object>>(instr);
         }
-    }
-
-    private string format_line_tab(int tab,string fmtstr,params object args[])
-    {
-        string rets ;
-        int i;
-        for (i=0;i<tab;i++) {
-            rets += "    ";
-        }
-        rets += String.Format(fmtstr,args);
-        rets += "\n";
-        return rets;
-    }
-
-    private string format_jarray_string(int tab, string name, JArray arr)
-    {
-    	string rets;
-    	int i;
-    	Object obj;
-    	rets = "";
-    	for (i=0;i<tab;i++) {
-    		rets += "    ";
-    	}
-    	rets += String.Format("\"%s\" : ", name);
-    	for (i=0;i< arr.Count;i++) {
-    		obj = arr[i];
-    		Console.Out.WriteLine("[{0}] = [{1}]", i, obj.GetType().FullName);
-    	}
-    	rets += "\n";
-    	return rets;
-
-    }
-
-    private string format_jobject_string(int tab,string name,JObject obj)
-    {
-    	string rets;
-    	int i;
-    	rets = "";
-        if (name.Length > 0) {
-            rets += this.format_line_tab(tab,);    
-        }
-        
-    	for (i=0;i<tab;i++) {
-    		rets += "    ";
-    	}
-
-    	if (name.Length > 0) {
-    		rets += String.Format("\"%s\" : ", name);
-    	}
-
-    	rets += "{\n";
-        rets += "}\n";
-    	return rets;
     }
 
 
@@ -88,7 +34,7 @@ class JsonUtil
         keys = new List<String>(this.m_obj.Keys);
         rets += "{\n";
         for (i=0;i<keys.Count;i++) {
-        	k = keys[i];
+            k = keys[i];
             rets += String.Format("\t\"{0}\" = type [{1}]\n", k, this.m_obj[k].GetType().FullName);
         }
         rets += "}\n";
@@ -100,7 +46,7 @@ class JsonUtil
         JsonUtil util ;
         int i;
         if (args.Length > 0) {
-        	i = 0;
+            i = 0;
             if (args[i] == "parse") {
                 for (i = 1; i < args.Length; i++) {
                     util = new JsonUtil(args[i], true);
