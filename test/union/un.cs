c5bbbd6da9c38630092378a8dfa6b0199d50a4d1
jeppeter <jeppeter@gmail.com>
2018-09-30 16:57:54 +0800

    to add union test

---
+++ test/union/un.cs
@@ -0,0 +1,38 @@
+using System.Runtime.InteropServices;
+using System;
+
+[StructLayoutAttribute(LayoutKind.Explicit)]
+struct SignedNumberWithText
+{
+[FieldOffsetAttribute(0)]
+public sbyte Num1;
+[FieldOffsetAttribute(0)]
+public short Num2;
+[FieldOffsetAttribute(0)]
+public int Num3;
+[FieldOffsetAttribute(0)]
+public long Num4;
+[FieldOffsetAttribute(0)]
+public float Num5;
+[FieldOffsetAttribute(0)]
+public double Num6;
+[FieldOffsetAttribute(16)]
+public string Text1;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SignedNumberWithText k;
+
+        k.Num5 = 0.0F;
+        k.Num6 = 0.0;
+        k.Num1 = 1;
+        Console.WriteLine("num1 {0}", k.Num1);
+        Console.WriteLine("Num5 {0}", k.Num5);
+        Console.WriteLine("Num6 {0}", k.Num6);
+        return;
+    }
+}
+
