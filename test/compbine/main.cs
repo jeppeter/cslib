a85da461b1382fba1624ee4837d0d15f6eca9773
jeppeter <jeppeter@gmail.com>
2018-11-18 19:45:15 +0800

    to add combine

---
+++ test/compbine/main.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+namespace Combine
+{
+	public class Combine
+	{
+		public static void Main(string[] args)
+		{
+			PrivateOne one = new PrivateOne();
+			Console.Out.WriteLine("Combine main");
+			return;
+		}
+	}
+}
---
+++ test/compbine/priv.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Combine
+{
+	class PrivateOne
+	{
+		public PrivateOne()
+		{
+			Console.Out.WriteLine("PrivateOne");
+		}
+	}
+}
