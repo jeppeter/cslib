7de682ba5a25d6792d8d18ddc71906bca68e69da
jeppeter <jeppeter@gmail.com>
2018-11-18 09:19:45 +0800

    to make framework with callfuncbyname with msbuild

---
+++ test/callfuncbyname/callfunc.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CallFuncExe
+{
+	public class CallFuncExe
+	{
+	public static void Main(string[] args)
+	{
+		return;
+	}		
+	}
+}
---
+++ test/callfuncbyname/dllfunc.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CallFuncDll
+{
+	public class CallFuncDll
+	{
+		public static void CallFuncOut(string name)
+		{
+			return;
+		}
+	}
+}
